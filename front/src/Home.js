3cafb57d462cf85101035a92e7a96c35762b7bc7
sklee1216 <54343936+sklee1216@users.noreply.github.com>
2023-04-22 20:00:49 -0400

    some styling on homepage

--- front/src/Home.js
+++ front/src/Home.js
@@ -1,38 +1,45 @@
 import React from "react";
-import {Link} from "react-router-dom";
-// importing Link from react-router-dom to navigate to 
-// different end points.
-  
+import { Link } from "react-router-dom";
+import "./Home.css";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Package Manager</h1>
-      <br />
-      <ul>
-        <li>
-          {/* Endpoint to route to Home component */}
-          <Link to="/Search">Search</Link>
+    <div className="home-container">
+      <h1 className="home-title">Package Manager</h1>
+      <ul className="home-nav">
+        <li className="home-nav-item">
+          <Link className="home-nav-link" to="/Search">
+            Search
+          </Link>
         </li>
-        <li>
-          {/* Endpoint to route to About component */}
-          <Link to="/upload">Upload</Link>
+        <li className="home-nav-item">
+          <Link className="home-nav-link" to="/upload">
+            Upload
+          </Link>
         </li>
-        <li>
-          {/* Endpoint to route to Contact Us component */}
-          <Link to="/update">Update</Link>
+        <li className="home-nav-item">
+          <Link className="home-nav-link" to="/update">
+            Update
+          </Link>
         </li>
-        <li>
-        <Link to="/delete">Delete</Link>
+        <li className="home-nav-item">
+          <Link className="home-nav-link" to="/delete">
+            Delete
+          </Link>
         </li>
-        <li>
-        <Link to="/create">create</Link>
+        <li className="home-nav-item">
+          <Link className="home-nav-link" to="/create">
+            Create
+          </Link>
         </li>
-        <li>
-        <Link to="/rate">rate</Link>
+        <li className="home-nav-item">
+          <Link className="home-nav-link" to="/rate">
+            Rate
+          </Link>
         </li>
       </ul>
     </div>
   );
 };
-  
+
 export default Home;
