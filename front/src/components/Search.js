0aaf5eda55b85e68118e7006bff4ba36dbea7a47
sklee1216 <54343936+sklee1216@users.noreply.github.com>
2023-04-23 15:41:49 -0400

    .

--- front/src/components/Search.js
+++ front/src/components/Search.js
@@ -1,7 +1,22 @@
 import React from "react";
+//import axios from 'axios'
 
 const Search = () => {
+        const payload = {
+          Version: "~4.18.2",
+          Name: "express",
+        };
     
+        fetch("http://localhost:8080/packages", {
+          method: "POST",
+          mode: "no-cors",
+          body: JSON.stringify([payload]),
+        })
+          .then((response) => response.json())
+          .then((data) => console.log(data)
+          .catch((error) => console.error(error)));
+      
+
     return (
         
         <div>
